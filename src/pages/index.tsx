2f50514075a5446ff1bd592ac8ccf8a8e7f86b96
Paul Ruales <paulruales1@gmail.com>
2022-01-09 22:41:07 -0500

    removing trpc, simplifying structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,7 @@
-import { trpc } from "@/utils/trpc";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  const { data, isLoading } = trpc.useQuery(["hello", { text: "paul" }]);
-
-  if (isLoading) return <div>Loading...</div>;
-
-  return <div className="font-bold"> {data && JSON.stringify(data)} </div>;
+  return <div className="font-bold"> HI </div>;
 };
 
 export default Home;
